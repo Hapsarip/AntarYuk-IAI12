a652ff099297b312fa3179a8dcfe66f0fe74e52e
ChristinaAngraeniP <panellah.a11christina@gmail.com>
2023-05-29 00:18:39 +0700

    Update input.js

--- src/input.js
+++ src/input.js
@@ -18,5 +18,32 @@ export default function input({ name, placeholder, values, setValues, value}) {
             break;
         default:
             type = "text";
-    };
+    }
+
+    return (
+        <div className="w-full mt-[10px] flex items-center gap-3 bg-[#FBAB05] opacity-30 rounded-xl px-8">
+          <input
+            required
+            name={name}
+            className=" font-Inter block w-full outline-0 h-[70px] bg-[#FBAB05] rounded-xl text-black font-bold text-lg "
+            type={name === "password" ? (pwdVisible ? "text" : "password") : type}
+            placeholder={placeholder}
+            autoComplete="off"
+            value={state}
+            onChange={handleChange}
+          />
+          {name === "password" && (
+            <button
+              type="button"
+              onClick={() => setPwdVisible((prevState) => !prevState)}
+            >
+              {pwdVisible ? (
+                <MdVisibilityOff size="1.25rem" className="flex-none" />
+              ) : (
+                <MdVisibility size="1.25rem" className="flex-none" />
+              )}
+            </button>
+          )}
+        </div>
+    );
 }
